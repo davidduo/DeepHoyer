e6f4ca3dde616e7cc13b07732c22c1e95536a8ec
rui.wu <rui.wu@horizon.ai>
2020-06-02 23:40:50 +0800

    lp

--- mnist/MLP/run.sh
+++ mnist/MLP/run.sh
@@ -1,7 +1,9 @@
-echo $(date +%F%n%T)
-python3 element.py --reg_type 5 --decay 0.001 --pretrained
-echo $(date +%F%n%T)
-python3 element.py --reg_type 5 --decay 0.0005 --pretrained
-echo $(date +%F%n%T)
-python3 element.py --reg_type 5 --decay 0.0001 --pretrained
-echo $(date +%F%n%T)
+python3 element.py --reg  0 --decay 0.0  
+
+#echo $(date +%F%n%T)
+#python3 element.py --reg_type 5 --decay 0.001 --pretrained
+#echo $(date +%F%n%T)
+#python3 element.py --reg_type 5 --decay 0.0005 --pretrained
+#echo $(date +%F%n%T)
+#python3 element.py --reg_type 5 --decay 0.0001 --pretrained
+#echo $(date +%F%n%T)
